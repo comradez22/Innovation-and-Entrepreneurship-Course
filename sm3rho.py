bce1713d44b28f15582f994389587d266bf1ddf7
张施珵 <108727329+comradez22@users.noreply.github.com>
2022-07-27 16:10:07 +0800

    Update sm3rho.py

--- sm3rho.py
+++ sm3rho.py
@@ -6,26 +6,30 @@
 
 import random
 import sm3
-def rho():
-    list_r_value = []
-    for i in range(pow(2,32)):
-        list_r = random.randint(0, pow(2,64))
-        m = TC(str(list_r))
-        M = Group(m)
-        Vn=SM3(M)
-        aa=""
-        for x in Vn:
-            aa += hex(x)[2:]
-        bb=aa[:1]
-        if(bb in list_r_value):
-            print("success")
-            break
-        else:
-            list_r_value.append(bb)
+
+def Rho_Attack(n):
+    a=random.randint(0,0xffff)
+    res=[]
+    for i in range(0xffff):
+        res.append(sm3(str(a))[:int(n/4)])
+        a=(2*a+1)
+        if(sm3(str(a))[:int(n/4)]in res):
+            print('Succeed')
+            return;
+    print('Failed')
 
 
-rho()
-
-
+if __name__ == '__main__':
+    time_start = time.time()
+    n=8
+    Rho_Attack(n)
+    time_end = time.time()
+    time_c = time_end - time_start
+    print("n=",n)
+    print('time cost', time_c, 's')
+    
+    
+    
+    
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
